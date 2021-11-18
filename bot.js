2ee7c4ec7d11d1ca5a2294586eebea2639fa3347
daveh566 <69384676+daveh566@users.noreply.github.com>
2021-11-18 15:17:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/* Copyright (C)2020 Yusuf Usta.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 WhatsAsena - Yusuf Usta
@@ -281,7 +281,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
             }
           }         
             return;                               
-    }
+    }         
 
         events.commands.map(
             async (command) =>  {
